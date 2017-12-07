3d2e0eff31060c5ab815c784c98e94762d1595eb
Gavin Howard <yzena.tech@gmail.com>
2017-12-07 16:10:50 -0700

    Get rid of the MySQL stuff and use a new one
    I already tested this new one with Yc. I am going to push this change
    and then do some style fixing.

--- README.md
+++ README.md
@@ -3,13 +3,6 @@
 This project contains all of the CMake tools for Yzena repos. It is usually
 included as a git submodule in other projects.
 
-## License
-
-Because some code in the repository comes from the
-[MySQL repo](https://github.com/mysql/mysql-server), and is licensed under the
-GNU General Public License, Version 2, all code in the repository is under that
-same license.
-
 ## Contents
 
 Files:
@@ -18,5 +11,3 @@ Files:
 	                 all of the other CMake files in the directory and adds all
 	                 subdirectories.
 	functions.cmake  All CMake functions used in Yzena repositories.
-	LICENSE.md       A Markdown version of the GNU General Public License,
-	                 Version 2.
--- functions.cmake
+++ functions.cmake
@@ -22,60 +22,199 @@
 #	KIND, either express or implied. See the Apache License for the specific
 #	language governing permissions and limitations under the Apache License.
 #
+#	The function merge_static_libs is under the following copyright and license:
+#	Copyright (C) 2012 Modelon AB
+#
+#	This program is free software: you can redistribute it and/or modify
+#	it under the terms of the BSD style license.
+#
+#	This program is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	FMILIB_License.txt file for more details.
+#
+#	You should have received a copy of the BSD license  along with this program.
+#	If not, contact Modelon AB <http://www.modelon.com>.
+#
 #	****** END LICENSE BLOCK ******
 
-function (create_test target)
+# Make sure we save this.
+set(SCRIPT_DIR "${CMAKE_CURRENT_SOURCE_DIR}")
+
+# Merge_static_libs(outlib lib1 lib2 ... libn) merges a number of static
+# libs into a single static library
+function(merge_static_libs outlib )
+
+	set(libs ${ARGV})
+	list(REMOVE_AT libs 0)
+
+	# Create a dummy file that the target will depend on
+	set(dummyfile ${CMAKE_CURRENT_BINARY_DIR}/${outlib}_dummy.c)
+	file(WRITE ${dummyfile} "// ${dummyfile}")
+
+	add_library(${outlib} STATIC ${dummyfile})
+
+	if("${CMAKE_CFG_INTDIR}" STREQUAL ".")
+		set(multiconfig FALSE)
+	else()
+		set(multiconfig TRUE)
+	endif()
+
+	# First get the file names of the libraries to be merged
+	foreach(lib ${libs})
+		get_target_property(libtype ${lib} TYPE)
+		if(NOT libtype STREQUAL "STATIC_LIBRARY")
+			message(FATAL_ERROR "Merge_static_libs can only process static libraries")
+		endif()
+		if(multiconfig)
+			foreach(CONFIG_TYPE ${CMAKE_CONFIGURATION_TYPES})
+				get_target_property("libfile_${CONFIG_TYPE}" ${lib} "LOCATION_${CONFIG_TYPE}")
+				list(APPEND libfiles_${CONFIG_TYPE} ${libfile_${CONFIG_TYPE}})
+			endforeach()
+		else()
+			get_target_property(libfile ${lib} LOCATION)
+			list(APPEND libfiles "${libfile}")
+		endif(multiconfig)
+	endforeach()
+	message(STATUS "will be merging ${libfiles}")
+
+	# Just to be sure: cleanup from duplicates
+	if(multiconfig)
+		foreach(CONFIG_TYPE ${CMAKE_CONFIGURATION_TYPES})
+			list(REMOVE_DUPLICATES libfiles_${CONFIG_TYPE})
+			set(libfiles ${libfiles} ${libfiles_${CONFIG_TYPE}})
+		endforeach()
+	endif()
+	list(REMOVE_DUPLICATES libfiles)
+
+	# Now the easy part for MSVC and for MAC
+	if(MSVC)
+		# lib.exe does the merging of libraries just need to conver the list into string
+		foreach(CONFIG_TYPE ${CMAKE_CONFIGURATION_TYPES})
+			set(flags "")
+			foreach(lib ${libfiles_${CONFIG_TYPE}})
+				set(flags "${flags} ${lib}")
+			endforeach()
+			string(TOUPPER "STATIC_LIBRARY_FLAGS_${CONFIG_TYPE}" PROPNAME)
+			set_target_properties(${outlib} PROPERTIES ${PROPNAME} "${flags}")
+		endforeach()
+
+	elseif(APPLE)
+		# Use OSX's libtool to merge archives
+		if(multiconfig)
+			message(FATAL_ERROR "Multiple configurations are not supported")
+		endif()
+		get_target_property(outfile ${outlib} LOCATION)
+		add_custom_command(TARGET ${outlib} POST_BUILD
+			COMMAND rm ${outfile}
+			COMMAND /usr/bin/libtool -static -o ${outfile}
+			${libfiles})
+	else()
+		# general UNIX - need to "ar -x" and then "ar -ru"
+		if(multiconfig)
+			message(FATAL_ERROR "Multiple configurations are not supported")
+		endif()
+		get_target_property(outfile ${outlib} LOCATION)
+		message(STATUS "outfile location is ${outfile}")
+		foreach(lib ${libfiles})
+			# objlistfile will contain the list of object files for the library
+			set(objlistfile ${lib}.objlist)
+			set(objdir ${lib}.objdir)
+			set(objlistcmake  ${objlistfile}.cmake)
+			# we only need to extract files once
+			if(${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/cmake.check_cache IS_NEWER_THAN ${objlistcmake})
+				#---------------------------------
+				configure_file("${SCRIPT_DIR}/merge.cmake.in" "${CMAKE_CURRENT_BINARY_DIR}/merge_${objlistfile}.cmake" @ONLY)
+				#---------------------------------
+				file(MAKE_DIRECTORY ${objdir})
+				add_custom_command(
+					OUTPUT ${objlistfile}
+					COMMAND ${CMAKE_COMMAND} -P ${objlistcmake}
+					DEPENDS ${lib})
+			endif()
+			list(APPEND extrafiles "${objlistfile}")
+			# relative path is needed by ar under MSYS
+			file(RELATIVE_PATH objlistfilerpath ${objdir} ${objlistfile})
+			add_custom_command(TARGET ${outlib} POST_BUILD
+				COMMAND ${CMAKE_COMMAND} -E echo "Running: ${CMAKE_AR} ru ${outfile} @${objlistfilerpath}"
+				COMMAND ${CMAKE_AR} ru "${outfile}" @"${objlistfilerpath}"
+				WORKING_DIRECTORY ${objdir})
+		endforeach()
+		add_custom_command(TARGET ${outlib}
+			POST_BUILD
+			COMMAND ${CMAKE_COMMAND} -E echo "Running: ${CMAKE_RANLIB} ${outfile}"
+			COMMAND ${CMAKE_RANLIB} ${outfile})
+	endif()
+	file(WRITE ${dummyfile}.base "const char* ${outlib}_sublibs=\"${libs}\";")
+	add_custom_command(
+		OUTPUT  ${dummyfile}
+		COMMAND ${CMAKE_COMMAND}  -E copy ${dummyfile}.base ${dummyfile}
+		DEPENDS ${libs} ${extrafiles})
+
+ endfunction()
+
+
+function(create_test target)
 
 	add_executable("${target}" "${target}.c")
 	target_link_libraries("${target}" "${ARGN}")
-	add_test (NAME ${target} COMMAND $<TARGET_FILE:${target}>)
+	add_test(NAME ${target} COMMAND $<TARGET_FILE:${target}>)
 
-endfunction (create_test)
+endfunction(create_test)
 
-function (create_shared_library name output_name src doinstall)
+function(create_shared_library name output_name src doinstall)
 
 	add_library("${name}" SHARED "${src}")
 	target_link_libraries("${name}" "${ARGN}")
 
-	set_target_properties("${name}" PROPERTIES OUTPUT_NAME "${output_name}")
+	set_target_properties("${name}"
+		PROPERTIES
+		LOCATION "${CMAKE_CURRENT_BINARY_DIR}"
+		OUTPUT_NAME "${output_name}")
 
-	if (doinstall)
+	if(doinstall)
 		install(TARGETS "${name}" LIBRARY DESTINATION lib/)
 	endif(doinstall)
 
-endfunction (create_shared_library)
+endfunction(create_shared_library)
 
-function (create_static_library name output_name src doinstall)
-
-	add_library("${name}" STATIC "${src}")
-	target_link_libraries("${name}" "${ARGN}")
-
-	set_target_properties("${name}" PROPERTIES OUTPUT_NAME "${output_name}")
-
-	if (doinstall)
-		install(TARGETS "${name}" ARCHIVE DESTINATION lib/)
-	endif(doinstall)
-
-endfunction (create_static_library)
-
-function (create_pic_library name output_name src doinstall)
+function(create_static_library name output_name src doinstall)
 
 	add_library("${name}" STATIC "${src}")
 	target_link_libraries("${name}" "${ARGN}")
 
-	set_target_properties("${name}" PROPERTIES OUTPUT_NAME "${output_name}")
-	set_property(TARGET "${name}" PROPERTY POSITION_INDEPENDENT_CODE ON)
+	set_target_properties("${name}"
+		PROPERTIES
+		LOCATION "${CMAKE_CURRENT_BINARY_DIR}"
+		OUTPUT_NAME "${output_name}")
 
-	if (doinstall)
+	if(doinstall)
 		install(TARGETS "${name}" ARCHIVE DESTINATION lib/)
 	endif(doinstall)
 
-endfunction (create_pic_library)
+endfunction(create_static_library)
 
-function (create_all_libraries shared_name static_name pic_name output_name src doinstall)
+function(create_pic_library name output_name src doinstall)
+
+	add_library("${name}" STATIC "${src}")
+	target_link_libraries("${name}" "${ARGN}")
+
+	set_target_properties("${name}"
+		PROPERTIES
+		LOCATION "${CMAKE_CURRENT_BINARY_DIR}"
+		OUTPUT_NAME "${output_name}"
+		POSITION_INDEPENDENT_CODE ON)
+
+	if(doinstall)
+		install(TARGETS "${name}" ARCHIVE DESTINATION lib/)
+	endif(doinstall)
+
+endfunction(create_pic_library)
+
+function(create_all_libraries shared_name static_name pic_name output_name src doinstall)
 
 	create_shared_library("${shared_name}" "${output_name}" "${src}" "${doinstall}" "${ARGN}")
 	create_static_library("${static_name}" "${output_name}" "${src}" "${doinstall}" "${ARGN}")
 	create_pic_library("${pic_name}" "${output_name}_pic" "${src}" "${doinstall}" "${ARGN}")
 
-endfunction (create_all_libraries)
+endfunction(create_all_libraries)
