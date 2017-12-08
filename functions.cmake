2668199b58833d224824b0f2ef46902d71b9b4c7
Gavin Howard <yzena.tech@gmail.com>
2017-12-07 22:31:48 -0700

    Attempt to fix a bug

--- functions.cmake
+++ functions.cmake
@@ -163,7 +163,7 @@ function(merge_static_libs outlib)
 			# relative path is needed by ar under MSYS
 			file(RELATIVE_PATH objlistfilerpath ${objdir} ${objlistfile})
 			add_custom_command(TARGET ${outlib} POST_BUILD
-				COMMAND ${CMAKE_COMMAND} -E echo "Running: ${CMAKE_AR} ru ${outfile} @${objlistfilerpath}"
+				COMMAND ${CMAKE_COMMAND} -E echo Running: ${CMAKE_AR} ru ${outfile} @${objlistfilerpath}
 				COMMAND ${CMAKE_AR} ru "${outfile}" @"${objlistfilerpath}"
 				WORKING_DIRECTORY ${objdir})
 		endforeach()
